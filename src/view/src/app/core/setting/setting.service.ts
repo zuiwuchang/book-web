da9a30abac893e2606e5a46c0fe9aa464cca881f
zuiwuchang <zuiwuchang@gmail.com>
2018-08-19 10:20:24 +0800

    save sidenav setting to localStorage

--- src/view/src/app/core/setting/setting.service.ts
+++ src/view/src/app/core/setting/setting.service.ts
@@ -6,16 +6,51 @@ import { Setting } from './setting';
 })
 export class SettingService {
   private setting: Setting = new Setting()
-  constructor() { }
+  constructor() {
+    if (this.getKey("isChapter") == "false") {
+      this.setting.Chapter = false;
+    } else {
+      this.setting.Chapter = true;
+    }
+    if (this.getKey("isHeader") == "false") {
+      this.setting.Header = false;
+    } else {
+      this.setting.Header = true;
+    }
+  }
+  private getKey(key: string): string {
+    if (typeof (Storage) == "undefined") {
+      console.log("not support storage")
+      return undefined;
+    }
+    return localStorage.getItem(key)
+  }
+  private setKey(key: string, val: string) {
+    if (typeof (Storage) == "undefined") {
+      console.log("not support storage")
+      return;
+    }
+    localStorage.setItem(key, val);
+  }
   // 返回 設置
   getSetting(): Setting {
     return this.setting;
   }
   toggleChapter() {
     this.setting.Chapter = !this.setting.Chapter;
+    if (this.setting.Chapter) {
+      this.setKey("isChapter", "true");
+    } else {
+      this.setKey("isChapter", "false");
+    }
   }
   toggleHeader() {
     this.setting.Header = !this.setting.Header;
+    if (this.setting.Header) {
+      this.setKey("isHeader", "true");
+    } else {
+      this.setKey("isHeader", "false");
+    }
   }
   updatePage(page: number, book?: string, chapter?: string) {
     this.setting.Page = page;
@@ -23,7 +58,7 @@ export class SettingService {
     this.setting.ChapterID = chapter;
   }
   updateFull(yes: boolean) {
-    if(this.setting.Full != yes){
+    if (this.setting.Full != yes) {
       this.setting.Full = yes;
     }
   }
