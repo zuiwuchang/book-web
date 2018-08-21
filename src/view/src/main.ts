016bd206c76f7ecd89e9da87d2233b769dc49e09
zuiwuchang <zuiwuchang@gmail.com>
2018-08-21 15:23:42 +0800

    update main.ts auto translations aot jit

--- src/view/src/main.ts
+++ src/view/src/main.ts
@@ -12,16 +12,17 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+let options = undefined;
+if (!environment.production) {
+  const translations = require(`raw-loader!./locale/zh-Hant.xlf`);
+  options = {
+    providers: [
+      { provide: TRANSLATIONS, useValue: translations },
+      { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
+    ]
+  };
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule, options)
   .catch(err => console.log(err));
-
-// //we use the webpack raw-loader to return the content as a string
-// const translations = require(`raw-loader!./locale/zh-Hant.xlf`);
-
-// platformBrowserDynamic().bootstrapModule(AppModule, {
-//   providers: [
-//     { provide: TRANSLATIONS, useValue: translations },
-//     { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
-//   ]
-// });
 
