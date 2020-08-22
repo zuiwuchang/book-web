3e347a630e36099a099ff45478246d421b491536
zuiwuchang <zuiwuchang@gmail.com>
2020-08-22 16:44:25 +0800

    fix file-upload not set token

--- view/src/app/markdown/dialog/file-upload/file-upload.component.ts
+++ view/src/app/markdown/dialog/file-upload/file-upload.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { OpenedBook } from 'src/app/core/settings/settings.service';
 import { FileUploader } from 'ng2-file-upload';
+import { SessionService } from 'src/app/core/session/session.service';
 const KB = 1024
 const MB = KB * 1024
 const GB = MB * 1024
@@ -13,6 +14,7 @@ const GB = MB * 1024
 export class FileUploadComponent implements OnInit {
   uploader: FileUploader
   constructor(
+    public readonly sessionService: SessionService,
     private readonly dialogRef: MatDialogRef<FileUploadComponent>,
     @Inject(MAT_DIALOG_DATA) private readonly data: OpenedBook
   ) {
@@ -20,6 +22,10 @@ export class FileUploadComponent implements OnInit {
   ngOnInit(): void {
     this.uploader = new FileUploader({
       url: "/api/v1/assets/upload/" + this.data.book + "/" + this.data.chapter,
+      headers: [{
+        name: 'token',
+        value: this.sessionService.token(),
+      }]
     })
   }
   onCancel() {
